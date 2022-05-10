016de43754b33f7ecc4e077f1b581b22c1a9cc3b
Lash Blast <evelyn.aguirre.9895@gmail.com>
2022-05-10 11:28:07 -0500

    verify save/edit functionality and text area size

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -80,7 +80,7 @@ router.post("/", withAuth, (req, res) => {
 router.put("/:id", withAuth, (req, res) => {
 	Post.update(
 		{
-			title: req.body.title,
+			post_text: req.body.post_text,
 		},
 		{
 			where: {
